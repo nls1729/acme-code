14a40380723344396808fabcdd5bf211b07f0de6
Norman L. Smith <nls1729@users.noreply.github.com>
2014-11-15 09:11:24 -0500

    Changed button to PanelMenu.Button to work with theme changes and max-win
    Signed-off-by: Norman L. Smith <nls1729@users.noreply.github.com>

--- lockscreen/extension.js
+++ lockscreen/extension.js
@@ -1,4 +1,5 @@
 
+// Copyright 2014 Donald Pakkies
 // Name: Lock Screen
 // Version: 0.1
 // License: GPL v3
@@ -6,61 +7,76 @@
 
 // Description: Gnome Lock Screen for LightDM Greeter
 
-// Modified at the request of Donald Pakkies to include
-// code to implement an acceleration key. 2014-11-14
+// 2014-11-14 Modified at the request of Donald Pakkies
+// to include code to implement an acceleration key. 
+// Ctrl+Alt+L executes dm-tool to lock screen.
 
 const St = imports.gi.St;
 const Gio = imports.gi.Gio;
 const Main = imports.ui.main;
 const Meta = imports.gi.Meta;
 const Shell = imports.gi.Shell;
+const Panel = imports.ui.panel;
+const PanelMenu = imports.ui.panelMenu;
+const Lang = imports.lang;
 const Util = imports.misc.util;
 
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 
 let settings;
-let button;
-let buttonSignal;
+let lockScreenPanelButton;
 
-function _activatelockscreen() {
-    Util.spawn(['/usr/bin/dm-tool', 'lock']);
-}
+const LockScreenPanelButton = new Lang.Class({
+    Name: 'LockScreenPanelButton',
+    Extends: PanelMenu.Button,
+
+    _init: function(settings) {
+        this.parent(0.0, null, true);
+        this.actor.name = 'lockScreenPanelButton';
+        this._buttonBin = new St.Bin();
+        this._icon = new St.Icon({icon_name: 'changes-prevent-symbolic',
+			        style_class: 'system-status-icon'});
+        this._buttonBin.set_child(this._icon);
+        this.actor.add_actor(this._buttonBin);
+    },
+
+    _activateLockScreen : function() {
+        Util.spawn(['/usr/bin/dm-tool', 'lock']);
+    },
+
+    _onButtonPress: function(actor, event) {
+        this._activateLockScreen();
+    },
+
+    destroy: function() {
+        this._icon.destroy();
+        this._buttonBin.destroy();
+        this.parent();
+    }
+});
 
 function init() {
-
     let GioSSS = Gio.SettingsSchemaSource;
     let schema = Me.metadata['settings-schema'];
     let schemaDir = Me.dir.get_path();
-    let schemaSource = GioSSS.new_from_directory(schemaDir, 
+    let schemaSource = GioSSS.new_from_directory(schemaDir,
                                          GioSSS.get_default(), false);
     let schemaObj = schemaSource.lookup(schema, false);
-    settings = new Gio.Settings( { settings_schema: schemaObj } );
-
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon ({ icon_name: 'changes-prevent-symbolic',
-			      style_class: 'system-status-icon'});
-
-    button.set_child(icon);
+    settings = new Gio.Settings({settings_schema: schemaObj});
 }
 
 function enable() {
-         
-    Main.wm.addKeybinding('acceleration-key', settings,
-                                  Meta.KeyBindingFlags.NONE,
-                                  Shell.KeyBindingMode.NORMAL |
-                                  Shell.KeyBindingMode.OVERVIEW,
-                                  _activatelockscreen);
-    buttonSignal = button.connect('button-press-event', _activatelockscreen);
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+    lockScreenPanelButton = new LockScreenPanelButton();
+    Main.wm.addKeybinding('lockscreen-acceleration-key', settings,
+                           Meta.KeyBindingFlags.NONE,
+                           Shell.KeyBindingMode.NORMAL |
+                           Shell.KeyBindingMode.OVERVIEW,
+                           lockScreenPanelButton._activateLockScreen);
+    Main.panel.addToStatusArea('lock-screen-panel-button',
+                                   lockScreenPanelButton, 0, 'right');
 }
 
 function disable() {
-    button.disconnect(buttonSignal);
-    Main.wm.removeKeybinding('acceleration-key');
-    Main.panel._rightBox.remove_child(button);
+    Main.wm.removeKeybinding('lockscreen-acceleration-key');
+    lockScreenPanelButton.destroy();
 }
