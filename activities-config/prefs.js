9c9d080c88f5fdb0f2cbaf18c44484e993c139f6
Norman L. Smith <nls1729@gmail.com>
2019-03-09 12:35:34 -0500

    activities-config prefs update
    Signed-off-by: Norman L. Smith <nls1729@gmail.com>

--- activities-config/prefs.js
+++ activities-config/prefs.js
@@ -46,7 +46,7 @@ const SHW_SPRED = "Spread Radius";
 const OVERR_USR = "Override Shell Theme";
 const SHOW_OVER = "Show Overview If No Applications Are Running";
 const POSITION  = "Move Activities to the Right";
-const COMMIT = "Commit: 772bc27f588bfe4df39ed502efb12a5d7ea05c10";
+const COMMIT = "Commit: bcfa2ffdc3508ebcc62611b8ddbd35309a13f956";
 const TILE_OFF = 'tile-max-effect-off';
 
 function init() {
